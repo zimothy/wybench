60f2940de6f9de5a8e11ef9bc6fe7ace2e17c90f
Timothy Jones <git@zimothy.com>
2011-10-14 06:00:14 +1300

    Correcting tab output.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NRUNS=1
-NRAMPUPS=0
+NRUNS=100
+NRAMPUPS=20
 
 bench="sum"
 i=1000
@@ -9,7 +9,7 @@ i=1000
 while [ $i -lt 100000 ]
 do
   java -cp "concurrent/micro/$bench" Gen $i | tee "concurrent/micro/$bench/gen.in" > /dev/null
-  echo -n "$i "
+  echo -en "$i\t"
   java -server -cp ".:$HOME/Projects/dave-whiley/lib/wyrt.jar:concurrent/micro/$bench" Runner -r$NRAMPUPS -n$NRUNS Main concurrent/micro/$bench/gen.in
   echo ""
   let i=i+1000
